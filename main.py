88f6a40ace28ebba064efe45cb9ea1b2fcb6d007
kolesnikovss <sergeykool37@gmail.com>
2020-11-18 09:50:32 +0700

    Исправление ошибки перевода текста в base64. В текст добавлялся перевод каретки, добавил метод rstrip

--- main.py
+++ main.py
@@ -6,58 +6,12 @@ from re import sub
 from pylibdmtx.pylibdmtx import encode as encode_dm
 import base64
 
-
 class App:
 
     def __init__(self):
-        def _onKeyRelease(event):
-            ctrl = (event.state & 0x4) != 0
-            if event.keycode == 65 and ctrl and event.keysym.lower() != "a":
-                event.widget.event_generate("<<SelectAll>>")
-            if event.keycode == 88 and ctrl and event.keysym.lower() != "x":
-                event.widget.event_generate("<<Cut>>")
-
-            if event.keycode == 86 and ctrl and event.keysym.lower() != "v":
-                event.widget.event_generate("<<Paste>>")
-
-            if event.keycode == 67 and ctrl and event.keysym.lower() != "c":
-                event.widget.event_generate("<<Copy>>")
-
-        def control_input_russian_char_txt_code(event):
-            if event.keysym=="??":
-                text=self.txt_code.get('1.0', END)
-                text=text[:-2]
-                self.txt_code.delete('1.0',END)
-                self.txt_code.insert('1.0',text)
-            return 'break'
-
-        def control_input_russian_char_gtin(event):
-            if event.keysym=="??":
-                text=self.txt_gtin.get()
-                text=text[:-1]
-                self.txt_gtin.delete(0,END)
-                self.txt_gtin.insert(0,text)
-            return 'break'
-
-        def control_input_russian_char_sn(event):
-            if event.keysym == "??":
-                text = self.txt_sn.get()
-                text = text[:-1]
-                self.txt_sn.delete(0, END)
-                self.txt_sn.insert(0, text)
-            return 'break'
-
-        def control_input_russian_char_gt_sn(event):
-            if event.keysym == "??":
-                text = self.txt_gt_sn.get()
-                text = text[:-1]
-                self.txt_gt_sn.delete(0, END)
-                self.txt_gt_sn.insert(0, text)
-            return 'break'
-
-
         def control_input_enter(event):
             self.txt_code.delete(END)
+            print(self.txt_code.get('1.0', END))
             return 'break'
 
         def control_input_space(event):
@@ -85,15 +39,18 @@ class App:
         def unable():
             self.txt_gtin.config(state=NORMAL)
             self.txt_sn.config(state=NORMAL)
-            self.txt_gt_sn.delete(0, END)
+            self.txt_gt_sn.delete(v)
             self.txt_gt_sn.config(state=DISABLED)
-
         def disable():
             self.txt_gt_sn.config(state=NORMAL)
-            self.txt_gtin.delete(0, END)
+            self.txt_gtin.delete(0,END)
             self.txt_gtin.config(state=DISABLED)
             self.txt_sn.delete(0, END)
             self.txt_sn.config(state=DISABLED)
+
+
+
+
 
         time = datetime.now()
         time = sub('\W+', '', str(time))[0:15]
@@ -101,64 +58,58 @@ class App:
         self.window.lift()
         self.window.geometry('600x600')
         self.window.title("Генератор DataMatrix")
-        self.window.bind_all("<Key>", _onKeyRelease, "+")
 
-        self.var = IntVar()
+        self.var=IntVar()
         self.var.set(0)
 
-        self.input_style_gtinsgtin = Radiobutton(text="GTIN SN", variable=self.var, value=0, command=unable)
+        self.input_style_gtinsgtin=Radiobutton(text="GTIN SN",variable=self.var,value=0,command=unable)
         self.input_style_gtinsgtin.pack()
-
         self.input_style_gtinsgtin.place(x=10, y=5)
 
-        self.input_style_gtinsn = Radiobutton(text="GTIN\SN", variable=self.var, value=1, command=disable)
+        self.input_style_gtinsn = Radiobutton(text="GTIN\SN", variable=self.var, value=1,command=disable)
 
         self.input_style_gtinsn.pack()
         self.input_style_gtinsn.place(x=110, y=5)
 
-        x0 = 0
-        y0 = 20
+        x0=0
+        y0=20
         self.lbl = Label(self.window, text="GTIN", font=("Arial Bold", 10))
-        self.lbl.place(x=10 + x0, y=10 + y0)
-        self.txt_gtin = Entry(self.window, width=20, font=("Arial Bold", 8), )
-        self.txt_gtin.place(x=80 + x0, y=10 + y0)
+        self.lbl.place(x=10+x0, y=10+y0)
+        self.txt_gtin = Entry(self.window,width=20, font=("Arial Bold", 8),)
+        self.txt_gtin.place(x=80+x0, y=10+y0)
         self.txt_gtin.focus()
-        self.txt_gtin.bind('<KeyRelease>', control_input_russian_char_gtin)
         self.txt_gtin.bind('<space>', control_input_space)
 
-        self.lbl = Label(self.window, text="SN", font=("Arial Bold", 10))
-        self.lbl.place(x=10 + x0, y=40 + y0)
-        self.txt_sn = Entry(width=20, font=("Arial Bold", 8))
 
-        self.txt_sn.place(x=80 + x0, y=40 + y0)
-        self.txt_sn.bind('<KeyRelease>', control_input_russian_char_sn)
+
+        self.lbl = Label(self.window, text="SN", font=("Arial Bold", 10))
+        self.lbl.place(x=10+x0, y=40+y0)
+        self.txt_sn = Entry(width=20, font=("Arial Bold", 8))
+        self.txt_sn.place(x=80+x0, y=40+y0)
         self.txt_sn.bind('<space>', control_input_space)
 
         self.lbl = Label(self.window, text="GTIN\SN", font=("Arial Bold", 10))
         self.lbl.place(x=10 + x0, y=65 + y0)
         self.txt_gt_sn = Entry(width=40, font=("Arial Bold", 8))
-        self.txt_gt_sn.event_add('<<Paste>>',
-                                 '<Control-igrave>')  # копипаст не зависимо какая стоит раскладка и какой копируется язык
         self.txt_gt_sn.place(x=80 + x0, y=65 + y0)
-        self.txt_gt_sn.bind('<KeyRelease>', control_input_russian_char_gt_sn)
         self.txt_gt_sn.bind('<space>', control_input_space)
         self.txt_gt_sn.config(state=DISABLED)
 
+
+
+
         self.lbl = Label(self.window, text="BARCODE", font=("Arial Bold", 10))
-        self.lbl.place(x=10 + x0, y=100 + y0)
+        self.lbl.place(x=10+x0, y=100+y0)
         self.txt_code = Text(height=3, width=40, font=("Arial Bold", 8))
-        self.txt_code.event_add('<<Paste>>',
-                                '<Control-igrave>')  # копипаст не зависимо какая стоит раскладка и какой копируется язык
-        self.txt_code.place(x=80 + x0, y=100 + y0)
-        self.txt_code.bind('<KeyRelease>',control_input_russian_char_txt_code)
+        self.txt_code.place(x=80+x0, y=100+y0)
         self.txt_code.bind("<Tab>", focus_next_window)
         self.txt_code.bind('<Return>', control_input_enter)
         self.txt_code.bind('<space>', control_input_space)
 
         self.lbl = Label(self.window, text="BASE64", font=("Arial Bold", 10))
         self.lbl.place(x=10 + x0, y=155 + y0)
-        self.txt_bs64 = Text(height=4, width=40, font=("Arial Bold", 8))
-        self.txt_bs64.place(x=80 + x0, y=155 + y0)
+        self.txt_bs64 = Text(height=3, width=40, font=("Arial Bold", 8))
+        self.txt_bs64.place(x=80 + x0, y=155+ y0)
         self.txt_bs64.config(state=DISABLED)
 
         self.btn_BC = Button(self.window, text="BarCode", command=self.barcode)
@@ -179,10 +130,10 @@ class App:
         self.txt_code.delete(1.0, END)
         gtin = self.txt_gtin.get()
         sn = self.txt_sn.get()
-        gtin_sn = self.txt_gt_sn.get()
-        if len(gtin) == 14 and len(sn) == 13:
-            barcode = f'01{gtin}21{sn}91ee0592pAY7tJHS6k1dwqQh6PKjTB8nmaVbBfuGpIx/FAcLMc4='
-        elif len(gtin_sn) == 27:
+        gtin_sn=self.txt_gt_sn.get()
+        if len(gtin)==14 and len(sn)==13:
+            barcode = f'01{gtin}21{sn}91EE06920NidnltXYq8A+0UOHQ0oFxPgpBdtVmLMvt4SbY6C0m4='
+        elif len(gtin_sn)==27:
             barcode = f'01{gtin_sn[0:14]}21{gtin_sn[14::]}91ee0592pAY7tJHS6k1dwqQh6PKjTB8nmaVbBfuGpIx/FAcLMc4='
         else:
             barcode = f'НЕКОРРЕКТНЫЙ БАРКОД'
@@ -190,12 +141,13 @@ class App:
 
     def data_matrix(self):
         self.name_image = generate_datamatrix(self.txt_code.get('1.0', END))
-
         self.txt_bs64.config(state=NORMAL)
-        self.txt_bs64.delete('1.0', END)
-        text = self.txt_code.get('1.0', END).encode("UTF-8")
-        text = base64.b64encode(text)
-        self.txt_bs64.insert(1.0, text.decode("UTF-8"))
+        text=self.txt_code.get('1.0', END).rstrip()
+        text=text.encode("utf-8")
+        text=base64.b64encode(text)
+        text=text.decode('utf-8')
+        self.txt_bs64.delete('1.0',END)
+        self.txt_bs64.insert(1.0,text)
         self.txt_bs64.config(state=DISABLED)
 
         canvas = Canvas(self.window, height=600, width=600)
@@ -223,7 +175,7 @@ def generate_datamatrix(code):
 
 test_code = \
     '0104601808010398210000000000RH691ee0592pAY7tJHS6k1dwqQh6PKjTB8nmaVbBfuGpIx/FAcLMc4='
-'0104601808010398210000000000RH691ee0592pAY7tJHS6k1dwqQh6PKjTB8nmaVbBfuGpIx/FAcLMc4='
+'0104601808010398210000000000RH691EE06928b0a1+3cl66k4Eay7I7zYb3EJGfRZvAEmUDYRWYLbac='
 app = App()
 "04601808010398"
 "0000000000RH6"
